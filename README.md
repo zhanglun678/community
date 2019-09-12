244c46e6d12ccd42ee585295e0755ffd209aaefe
zhanglun <zhanglun678@163.com>
2019-09-12 12:32:42 +0800

    extract the code to the configuration file

--- README.md
+++ README.md
@@ -6,7 +6,9 @@
 
 [spring boot web文档](https://spring.io/guides/gs/serving-web-content/)
 
-[github oauth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+[creating-an-oauth-app](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
+[authorizing-oauth-apps](https://developer.github.com/apps/building-oauth-apps/authorizing-oauth-apps/)
 
 [okhttp](https://square.github.io/okhttp/)
 ## 工具
--- src/main/java/com/zl/controller/AuthorizeController.java
+++ src/main/java/com/zl/controller/AuthorizeController.java
@@ -1,6 +1,7 @@
 package com.zl.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +21,21 @@ public class AuthorizeController {
 	@Autowired
 	private GithubProvider githubProvider;
 
+	@Value("${github.client.id}")
+	private String client_id;
+	@Value("${github.client.redirect_uri}")
+	private String redirect_uri;
+	@Value("${github.client.client_secret}")
+	private String client_secret;
+
 	@RequestMapping("/callback")
 	public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
 		AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
 		accessTokenDTO.setCode(code);
-		accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+		accessTokenDTO.setRedirect_uri(redirect_uri);
 		accessTokenDTO.setState(state);
-		accessTokenDTO.setClient_id("688182fe5882ee89f01c");
-		accessTokenDTO.setClient_secret("7e11ec36a2e7f0ae42fb1c814da0f0338746bda4");
+		accessTokenDTO.setClient_id(client_id);
+		accessTokenDTO.setClient_secret(client_secret);
 		String access_token = githubProvider.getAccessToken(accessTokenDTO);
 		GithubUser user = githubProvider.getUser(access_token);
 		System.out.println(user.getId());
